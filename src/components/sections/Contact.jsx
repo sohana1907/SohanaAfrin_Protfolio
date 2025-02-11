8d4cd5fffedf793c505a51cc67514585f83c4b80
apeksha-rgb <apeksha.manshi.shakya@gmail.com>
2025-02-11 22:47:05 +0530

    Made form responsive

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -32,7 +32,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             {" "}
             Get In Touch
