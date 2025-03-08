3f2b98dc8b790c21c408fb1a635bd47c0d4414cf
arbinzaman <arbinzmn@gmail.com>
2025-03-09 01:14:13 +0600

    feat: added latest resume of sohana afrijn

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -17,10 +17,10 @@ export const Home = () => {
           </p>
           <div className="flex justify-center space-x-4">
             <a
-              href="#projects"
+              href="https://drive.google.com/file/d/1sNuwUbMEqhPYgU8hhhIzScO1yteKpal-/view?usp=sharing"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
-              View Projects
+              resume
             </a>
 
             <a
